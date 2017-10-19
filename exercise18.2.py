fd9fad57d8318c281c82360a74c9304fa121fa5a
Philip Ulrich <philip@ulrich.life>
2017-10-19 22:48:53 +0000

    first commit

---
+++ exercise18.2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+###############################################################################
+#
+print("\nExercise 18.2\n")
+#
+# Question 1
+# 1. Write a Deck method called deal_hands that takes two parameters, the number
+# of hands and the number of cards per hand. It should create the appropriate 
+# number of Hand objects, deal the appropriate number of cards per hand, and 
+# return a list of Hands.
+#
